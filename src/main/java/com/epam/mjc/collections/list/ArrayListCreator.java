6e6aa335c150a6febf8d4631e7f523e075f299ee
odilhon85 <i.o.r@inbox.ru>
2022-09-30 11:16:16 +0500

    1-attempt

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,15 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> respondList = new ArrayList<>();
+        int i=1;
+        for (String s:sourceList) {
+            if(i%3==0){
+                respondList.add(s);
+                respondList.add(s);
+            }
+            i++;
+        }
+        return respondList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,42 @@
 package com.epam.mjc.collections.list;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        ListComparator comparator = new ListComparator();
+        Collections.sort(sourceList, comparator);
+        System.out.println(sourceList);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int response = 0;
+        int aElement = calculate(a);
+        int bElement = calculate(b);
+
+        if(aElement > bElement){
+            response = 1;
+        }else if(aElement < bElement){
+            response = -1;
+        }else{
+            int aInt = Integer.parseInt(a);
+            int bInt = Integer.parseInt(b);
+            if(aInt > bInt){
+                response = 1;
+            }else if(aInt < bInt){
+                response = -1;
+            }
+        }
+        return response;
+    }
+
+    public int calculate(String s){
+        int x = Integer.parseInt(s);
+        return (5*(x*x)+3);
     }
 }
